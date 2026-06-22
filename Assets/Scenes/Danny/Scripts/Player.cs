af289bdfb31cd97b8df231fac4c62a5a4ca13252
Blueis40879 <40879@ma-web.nl>
2026-06-22 14:45:17 +0200

    animation!

--- Assets/Scenes/Danny/Scripts/Player.cs
+++ Assets/Scenes/Danny/Scripts/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour
     public float horizontalSpeed = 3f;
     Rigidbody2D rb;
     BoxCollider2D boxCollider;
+    private bool facingRight = true;
+    [SerializeField] private Animator animator;
 
     void Start()
     {
@@ -17,10 +19,32 @@ public class Player : MonoBehaviour
         
         float horizontal = Input.GetAxis("Horizontal");
 
-
         
         transform.Translate(transform.right * horizontalSpeed * horizontal * Time.deltaTime);
+        FlipCharacter(horizontal);
 
+        if (horizontal != 0)
+        {
+            animator.SetBool("IsMoving", true);
+        }
+        else
+        {
+            animator.SetBool("IsMoving", false);
+        }
         
     }
+
+
+    private void FlipCharacter(float horizontal)
+{
+    if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
 }
+}
+
+
